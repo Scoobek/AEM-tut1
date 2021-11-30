628b27e477b131347d84bb86ec25e7bcfb1514c7
Wojciech <wojciech.skubisz@gmail.com>
2021-11-30 12:11:10 +0100

    start

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
@@ -0,0 +1,42 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.settings.SlingSettingsService;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import java.util.Optional;
+
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class HelloWorldModel {
+    private String message = "123";
+    @Inject
+    private String text;
+
+    @PostConstruct
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
